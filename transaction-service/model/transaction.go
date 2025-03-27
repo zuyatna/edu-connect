0b4cb4d898ee13bef9765e4c0a968e64ed8b63bd
Suyatna <zuyatna@gmail.com>
2025-03-27 17:47:02 +0700

    fix: fund collect

--- transaction-service/model/transaction.go
+++ transaction-service/model/transaction.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/google/uuid"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"gorm.io/gorm"
 )
 
 type Transaction struct {
@@ -59,10 +60,13 @@ type Post struct {
 }
 
 type FundCollect struct {
-	FundCollectID uuid.UUID `json:"fund_collect_id" gorm:"type:uuid;primaryKey;default:gen_random_uuid()"`
-	PostID        uuid.UUID `json:"post_id" gorm:"type:uuid; not null"`
-	UserID        string    `json:"user_id" gorm:"type:varchar(255); not null"`
-	UserName      string    `json:"user_name" gorm:"type:varchar(255); not null"`
-	Amount        float64   `json:"amount" gorm:"type:float; not null"`
-	TransactionID string    `json:"transaction_id" gorm:"type:varchar(255); not null"`
+	FundCollectID uuid.UUID      `json:"fund_collect_id" gorm:"type:uuid;primaryKey;default:gen_random_uuid()"`
+	PostID        uuid.UUID      `json:"post_id" gorm:"type:uuid; not null"`
+	UserID        string         `json:"user_id" gorm:"type:varchar(255); not null"`
+	UserName      string         `json:"user_name" gorm:"type:varchar(255); not null"`
+	Amount        float64        `json:"amount" gorm:"type:float; not null"`
+	TransactionID string         `json:"transaction_id" gorm:"type:varchar(255); not null"`
+	CreatedAt     time.Time      `json:"created_at" gorm:"type:timestamp; not null; autoCreateTime"`
+	UpdatedAt     time.Time      `json:"updated_at" gorm:"type:timestamp; not null; autoUpdateTime"`
+	DeletedAt     gorm.DeletedAt `json:"deleted_at" gorm:"type:timestamp"`
 }
